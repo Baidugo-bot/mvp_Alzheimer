b5ec551e142394c86d55d2c563d81b22fe805512
FeliciaShih <fefitinhah@gmail.com>
2022-05-24 22:56:20 -0300

    Save casa

--- mvp_alzheimer_projeto/lib/src/views/LocalizationView.dart
+++ mvp_alzheimer_projeto/lib/src/views/LocalizationView.dart
@@ -28,79 +28,85 @@ class LocalizationViewState extends State<LocalizationView> {
     bool isMapReady = false;
 
 
-    void updateLocalization(LatLng value){
-      LocalizationController.instance.mapController.move(value, 15.9);
-      isMapReady=true;
-    }
 
-    return Scaffold(// map dos botoes
-      backgroundColor: AppController.instance.mainColor,
-      appBar: AppBar(
+    print("iniciado: "+isMapReady.toString());
+    return StatefulBuilder(builder: (context,setState){
+      return Scaffold(// map dos botoes
         backgroundColor: AppController.instance.mainColor,
-        title: Center(child: Text("Map Page ")),
-        actions: [
-          InkWell(
-            onTap: (){
-              setState(() {
-                LocalizationController.instance.getCurrentLocation().then(
-                        (value) => updateLocalization(value));
+        appBar: AppBar(
+          backgroundColor: AppController.instance.mainColor,
+          title: Center(child: Text("Map Page ")),
+          actions: [
+            InkWell(
+              onTap: (){
+                void updateLocalization(LatLng value){
 
-              });
-            },
-            child: Icon(Icons.refresh),
-          )
-        ],
-      ),
-      body: FlutterMap(
-        mapController:  (isMapReady)?LocalizationController.instance.mapController:null,
-        options: MapOptions(
-          center: LatLng(lat, lng),
-          zoom: 15.9,
+                  setState(() {
+                    LocalizationController.instance.mapController.move(value, 15.9);
+                    isMapReady=true;
+                  });
+
+                }
+                setState(() {
+                  LocalizationController.instance.getCurrentLocation().then(
+                          (value) => LocalizationController.instance.mapController.onReady.then((result) =>updateLocalization(value)));
+                });
+              },
+              child: Icon(Icons.refresh),
+            )
+          ],
         ),
-        layers: [
-          TileLayerOptions(
-            urlTemplate: "https://api.mapbox.com/styles/v1/hugoomori/cl352g958002f16pjtd35538m/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaHVnb29tb3JpIiwiYSI6ImNsMzUxODZ1bjA3d3Mza21wOThvbDYxejYifQ.nnHPv3NEXheUbl30Hky1kQ",
-            additionalOptions: {
-              'accessToken': 'pk.eyJ1IjoiaHVnb29tb3JpIiwiYSI6ImNsMzUxODZ1bjA3d3Mza21wOThvbDYxejYifQ.nnHPv3NEXheUbl30Hky1kQ',
-              'id': 'mapbox.mapbox-streets-v8'
-            },
-            attributionBuilder: (_) {
-
-              return Text("© OpenStreetMap contributors");
-            },
+        body: FlutterMap(
+          mapController:  (isMapReady)?LocalizationController.instance.mapController:null,
+          options: MapOptions(
+            center: LatLng(lat, lng),
+            zoom: 15.9,
           ),
-          MarkerLayerOptions(
-            markers: [
-              Marker(
-                width: 24.0,
-                height: 24.0,
-                point: (isMapReady)?LocalizationController.instance.mapController.center:LatLng(lat, lng),
-                builder: (ctx) =>
-                    Row(
-                      children: [
-                        Icon(Icons.square,color: Colors.blue,),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: "https://api.mapbox.com/styles/v1/hugoomori/cl352g958002f16pjtd35538m/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaHVnb29tb3JpIiwiYSI6ImNsMzUxODZ1bjA3d3Mza21wOThvbDYxejYifQ.nnHPv3NEXheUbl30Hky1kQ",
+              additionalOptions: {
+                'accessToken': 'pk.eyJ1IjoiaHVnb29tb3JpIiwiYSI6ImNsMzUxODZ1bjA3d3Mza21wOThvbDYxejYifQ.nnHPv3NEXheUbl30Hky1kQ',
+                'id': 'mapbox.mapbox-streets-v8'
+              },
+              attributionBuilder: (_) {
 
-                      ],
-                    )
-              ),
-              Marker(
-                  width: 24.0,
-                  height: 24.0,
-                  point: (isMapReady)?LocalizationController.instance.mapController.center:LatLng(lat, lng),
-                  builder: (ctx) =>
-                      Row(
-                        children: [
-                          Icon(Icons.circle,color: Colors.lightBlueAccent),
+                return Text("© OpenStreetMap contributors");
+              },
+            ),
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                    width: 24.0,
+                    height: 24.0,
+                    point: (isMapReady)?LocalizationController.instance.mapController.center:LatLng(lat, lng),
+                    builder: (ctx) =>
+                        Row(
+                          children: [
+                            Icon(Icons.square,color: Colors.blue,),
 
-                        ],
-                      )
-              ),
-            ],
-          ),
-        ],
-      ),
+                          ],
+                        )
+                ),
+                Marker(
+                    width: 24.0,
+                    height: 24.0,
+                    point: (isMapReady)?LocalizationController.instance.mapController.center:LatLng(lat, lng),
+                    builder: (ctx) =>
+                        Row(
+                          children: [
+                            Icon(Icons.circle,color: Colors.lightBlueAccent),
 
-    );
+                          ],
+                        )
+                ),
+              ],
+            ),
+          ],
+        ),
+
+      );
+    });
 
   }
 
