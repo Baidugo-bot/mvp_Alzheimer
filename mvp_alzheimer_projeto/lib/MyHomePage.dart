ae50d2e835ef6ce358fe110f99e651e1fa86fed0
davidcr4p <davidcr4p@gmail.com>
2022-06-02 19:31:17 -0300

    Código quase pronto

--- mvp_alzheimer_projeto/lib/MyHomePage.dart
+++ mvp_alzheimer_projeto/lib/MyHomePage.dart
@@ -5,11 +5,8 @@ import 'package:flutter/rendering.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:projeto_estudo/AppController.dart';
 
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-
   final String title;
 
   @override
@@ -18,16 +15,25 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final myInstance = AppController.instance;
-  final style = ElevatedButton.styleFrom(primary: Colors.white,fixedSize: Size(120, 70));
+  final style =
+      ElevatedButton.styleFrom(primary: Colors.white, fixedSize: Size(120, 70));
   //light style theme =
   final defaultPadding = 20.0;
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      
+      // map dos botoes
+      
 
-    return Scaffold(// map dos botoes
       appBar: AppBar(
-        title: Center(child: Text("Dia "+myInstance.now.day.toString()+" - "+ Jiffy(myInstance.now).EEEE.toString())),
+        automaticallyImplyLeading: false,
+        title: Center(
+            child: Text("Dia " +
+                myInstance.now.day.toString() +
+                " - " +
+                Jiffy(myInstance.now).EEEE.toString())),
       ),
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
@@ -38,40 +44,56 @@ class _MyHomePageState extends State<MyHomePage> {
               backgroundImage: AssetImage('assets/images/pelezin.jpg'),
               radius: 100,
             ),
-            Text("Pele",style: TextStyle(fontSize: 20),),
-            Text("Nascimento: 05/10/2001",style: TextStyle(fontSize: 20),),
-            Text("Idade: 35 anos",style: TextStyle(fontSize: 20),),
-            Container(height: 80,),
+            Text(
+              "Pele",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "Nascimento: 05/10/2001",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              "Idade: 35 anos",
+              style: TextStyle(fontSize: 20),
+            ),
+            Container(
+              height: 80,
+            ),
             Row(
-
               mainAxisAlignment: MainAxisAlignment.center,
-
               children: [
                 ElevatedButton(
                   style: style,
                   onPressed: () {
                     Navigator.of(context).pushNamed('/reminders');
                     print("teste");
+                    automaticallyImplyLeading:
+                    false;
                   },
-                  child: Text("LEMBRETES",style: TextStyle(color: Colors.black),),
-
-
+                  child: Text(
+                    "LEMBRETES",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
-                Container(width: defaultPadding,),
+                Container(
+                  width: defaultPadding,
+                ),
                 ElevatedButton(
                   style: style,
                   onPressed: () {
                     Navigator.of(context).pushNamed('/memories');
                     print("teste");
                   },
-                  child: Text("MEMORIA",style: TextStyle(color: Colors.black),),
+                  child: Text(
+                    "MEMORIA",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
               ],
             ),
             Container(height: 10),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-
               children: [
                 ElevatedButton(
                   style: style,
@@ -79,65 +101,48 @@ class _MyHomePageState extends State<MyHomePage> {
                     Navigator.of(context).pushNamed('/family');
                     print("teste");
                   },
-                  child: Text("FAMILIA",style: TextStyle(color: Colors.black),),
-
-
+                  child: Text(
+                    "FAMILIA",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
-                Container(width: defaultPadding,),
+                Container(
+                  width: defaultPadding,
+                ),
                 ElevatedButton(
                   style: style,
                   onPressed: () {
                     Navigator.of(context).pushNamed('/profile');
                     print("teste");
                   },
-                  child: Text("PERFIL",style: TextStyle(color: Colors.black),),
+                  child: Text(
+                    "PERFIL",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
               ],
             ),
             Container(height: 10),
             Row(
-
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-
                 ElevatedButton(
-
-                  style: ElevatedButton.styleFrom(primary: Colors.white,fixedSize: Size(260, 70) ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.white, fixedSize: Size(260, 70)),
                   onPressed: () {
                     Navigator.of(context).pushNamed('/configurations');
                     print("teste");
                   },
-                  child: Text("CONFIGURACAO",style: TextStyle(color: Colors.black),),
-
-
+                  child: Text(
+                    "CONFIGURACAO",
+                    style: TextStyle(color: Colors.black),
+                  ),
                 ),
               ],
             ),
           ],
         ),
       ),
-  //     floatingActionButton:  Container(
-  //       height: 90.0,
-  //       width: 90.0,
-  //
-  //       child: FloatingActionButton(
-  //         shape: BeveledRectangleBorder(
-  //             borderRadius: BorderRadius.zero
-  //         ),
-  //         backgroundColor: Colors.greenAccent,
-  //         onPressed: () {
-  //
-  //         },
-  //         child: Text('''+ Adicionar
-  // lembretes
-  //         '''),
-  //
-  //       ),
-  //     ),
-
     );
   }
-
-
-
 }
--- mvp_alzheimer_projeto/lib/src/views/ProfileView.dart
+++ mvp_alzheimer_projeto/lib/src/views/ProfileView.dart
@@ -1,16 +1,84 @@
 import 'package:flutter/material.dart';
 
-class ProfileView extends StatelessWidget {
+import 'package:jiffy/jiffy.dart';
+import 'package:projeto_estudo/AppController.dart';
+import 'package:projeto_estudo/src/views/EditProfileView.dart';
 
+class ProfileView extends StatefulWidget {
+  String nome = "";
+  int data = 0;
+  ProfileView();
+  @override
+  _ProfileViewState createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  // debugShowCheckedModeBanner: false;
   @override
   Widget build(BuildContext context) {
+    DateTime agora = DateTime.now();
+    print(
+        (agora.difference(AppController.instance.profile.dataNasc).inDays / 365)
+            .toString()
+            .substring(0, 2));
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Perfil"),
+        automaticallyImplyLeading: false,
+        title: Text(" Perfil"),
+        actions: [
+//botão editar perfil
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return EditProfile();
+              }));
+            },
+            child: Icon(
+              Icons.edit,
+              color: Colors.red,
+              size: 30.0,
+            ),
+          ),
+//botão volta menu principal
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/');
+            },
+            child: Icon(
+              Icons.keyboard_return,
+              color: Colors.red,
+              size: 30.0,
+            ),
+          )
+        ],
       ),
-      body: Text(
-          "Estou na Profile view"
-
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+//informações tela principal
+          Center(
+              child: CircleAvatar(
+            backgroundImage: AppController.instance.profile.image,
+            radius: 100,
+          )),
+          Text(
+            "Nome: ${AppController.instance.profile.nome}",
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            "Nascimento: ${Jiffy(AppController.instance.profile.dataNasc).format("MMMM do yyyy")}",
+            style: TextStyle(fontSize: 20),
+          ),
+          Text(
+            "Idade: ${(agora.difference(AppController.instance.profile.dataNasc).inDays / 365).toString().substring(0, 2)}",
+            style: TextStyle(fontSize: 20),
+          ),
+          Container(
+            height: 80,
+          ),
+        ],
       ),
     );
   }
