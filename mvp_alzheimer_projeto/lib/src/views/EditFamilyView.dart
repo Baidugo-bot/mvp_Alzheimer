97a6c839f17d202a6b101a17a7af06cbb7a08537
davidcr4p <davidcr4p@gmail.com>
2022-10-28 10:34:52 -0300

    erro1

--- mvp_alzheimer_projeto/lib/src/views/EditFamilyView.dart
+++ mvp_alzheimer_projeto/lib/src/views/EditFamilyView.dart
@@ -1,85 +1,54 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:projeto_estudo/src/components/AddFamilyComponents.dart';
-import 'package:projeto_estudo/src/controller/EditFamilyController.dart';
+import 'package:projeto_estudo/AppController.dart';
+import 'package:projeto_estudo/src/models/FamilyModel.dart';
+import '../components/AddFamilyComponents.dart';
+import '../components/CustomButton.dart';
 import '../components/EditFamilyComponetes.dart';
 import '../controller/EditFamilyController.dart';
-import '../models/FamilyModel.dart';
+
 class EditFamilyView extends StatefulWidget {
+  const EditFamilyView({Key? key}) : super(key: key);
+
   @override
   EditFamilyViewState createState() => EditFamilyViewState();
 }
 
 class EditFamilyViewState extends State<EditFamilyView> {
-  TextEditingController txtNome = TextEditingController();
-    TextEditingController txtParentesco = TextEditingController();
-  TextEditingController txtTelephone = TextEditingController();
-  DateTime dataController = DateTime.now();
-
-  get args => null;
-
-  
-
-  get imageController => null;
-
-  void Salvar() {
-    setState(() {
-      TextEditingController titleController =  TextEditingController();
-      TextEditingController descController = TextEditingController();
-      EditFamilyController.instance.nome = txtNome.text;
-      EditFamilyController.instance.parentesco = txtParentesco.text;
-      EditFamilyController.instance.dataNasc = dataController;
-      EditFamilyController.instance.image =
-      EditFamilyController.instance.imagemTemporaria;
-      FamilyModel.instance.family.add(new Family(
-        title: txtNome.text,
-        date: dataController,
-        Telephone: int.parse(txtTelephone.text),
-      ));
-    });
-  }
-
+  /*
+  *  new AlertDialog(
+        title: new Text('Are you sure?'),
+        content: new Text('Do you want to exit an App'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: new Text('No'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: new Text('Yes'),
+          ),
+        ],
+      ),
+  * */
   @override
   Widget build(BuildContext context) {
-    // final ImagePicker picker = ImagePicker();
-    // XFile? Img;
+    var args = ModalRoute.of(context)!.settings.arguments as Map<String,Family>;
+    TextEditingController titleController = new TextEditingController();
+    TextEditingController descController = new TextEditingController();
+    DateTime dateController = DateTime.now() ;
+    String imageLink;
 
+    titleController.text = args["family"]!.getTitle();
+    dateController = args["family"]!.getDate();
+    descController.text = args["family"]!.getDescription();
+    imageLink = args['family']!.imgLink?? "assets/images/imagemEscolha.png";
+    print("Modify: "+args['family']!.getIdentifier().toString());
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
-      appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
-        elevation: 0,
-        centerTitle: true,
-        automaticallyImplyLeading: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-//botão volta menu principal
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, '/seeFamily');
-              },
-              child: Icon(
-                Icons.keyboard_return,
-                color: Color.fromARGB(255, 255, 255, 255),
-                size: 40.0,
-              ),
-            ),
-            Text(
-              " Eitar Familiar",
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-//botão add perfil
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButton(icon: Icon(null), onPressed: () {}),
-                ]),
-          ],
-        ),
-      ),
 
+      backgroundColor: AppController.instance.mainColor,
+      appBar: CustomAppBar.instance.getDefault(context,"/memories"),
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -95,12 +64,22 @@ class EditFamilyViewState extends State<EditFamilyView> {
                   bottom: BorderSide(width: 2, color: Colors.grey),
                 ),
               ),
-              child: GestureDetector(
-                onTap: () {},
-                // child: Image(
-                //   image: args["family"]!.getImage(),
-                //   height: 250,
-                // ),
+              child: ImagePickerContainer(
+                imageLink : imageLink,
+                response: () async {
+                  imageLink =  await AppController.instance.getImage().then((value) => imageLink = value);
+                  Navigator.of(context).pushNamed('/Family', arguments: {
+                    'family': new Family(
+                        title: titleController.text,
+                        date: dateController,
+                        description: descController.text,
+                        identifier: args['family']!.getIdentifier(),
+                        image: FileImage(File(imageLink)),
+                        imgLink: imageLink
+                    ),
+
+                  });
+                },
               ),
             ),
             Container(
@@ -112,14 +91,11 @@ class EditFamilyViewState extends State<EditFamilyView> {
               myResult: titleController,
 
             ),
-            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.lightBlue,
-                              ),
-                              child: DateBorderedField(
-                                onChangeFunction: (DateTime date) {dataController = date;},
-                              ),
-                            ),
+            DateBorderedField(
+              initialValue: dateController,
+              onChangeFunction: (DateTime date){dateController = date;},
+              lastDate: dateController,
+            ),
             BorderedTextField(
               title: "Anotacoes:",
               haveFrame: true,
@@ -132,22 +108,26 @@ class EditFamilyViewState extends State<EditFamilyView> {
                 CustomButton(
                   title: "Salvar",
                   color: Colors.green,
-                  response: (){
-                if(titleController.text=="" || descController.text==""){
-                  print("Preencha");
-                }else{
+                  response: () async {
+                    if(titleController.text=="" || descController.text==""){
+                      print("Preencha");
+                    }else{
 
-                  FamilyModel.instance.family.add(new Family(
-                      title: titleController.text, date: DateTime.now(),
-                      description: descController.text,
-                      identifier: FamilyModel.instance.family.length,
-                    image: imageController, Telephone: 0));
-                  Navigator.of(context).pushNamed('/family', arguments: {});
-                }
-
-
-
-              },
+                      bool imgExists = false;
+                      await File(imageLink).exists().then((value) =>imgExists=value);
+                      ImageProvider<Object>? finalImg = ((imgExists)?FileImage(File(imageLink)):AssetImage("assets/images/imagemEscolha.png")) as ImageProvider<Object>?;
+                      EditFamilyController.instance.changeById(args["family"]!.getIdentifier(),
+                            Family(
+                                title: titleController.text,
+                                date: dateController,
+                                description: descController.text,
+                                identifier: args["family"]!.getIdentifier(),
+                                image: finalImg ?? AssetImage("assets/images/imagemEscolha.png"),
+                                imgLink: (imgExists)?imageLink:"assets/images/imagemEscolha.png"
+                            ));
+                        Navigator.of(context).pushNamed('/memories', arguments: {});
+                    }
+                  },
                 ),
                 Container(width: 10,),
                 CustomButton(
@@ -155,7 +135,7 @@ class EditFamilyViewState extends State<EditFamilyView> {
                   color: Colors.red,
                   response: (){
                     EditFamilyController.instance.excludeById(args["family"]!.getIdentifier());
-                    Navigator.of(context).pushNamed('/family', arguments: {});
+                    Navigator.of(context).pushNamed('/memories', arguments: {});
                   },
                 ),
               ],
@@ -165,17 +145,4 @@ class EditFamilyViewState extends State<EditFamilyView> {
       ),
     );
   }
-
-  RaisedButton(
-      {required Text child,
-      required Color color,
-      required void Function() onPressed}) {}
 }
-//  _getFromGallery()  {
-    
-//     if (pickedFile != null) {
-//       setState(() {
-//         imageFile = File(pickedFile.path);
-//       });
-//     }
-//   }
