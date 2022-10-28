08fd901e35e457fc7339308b6f398aa9fce94e06
davidcr4p <davidcr4p@gmail.com>
2022-10-28 17:12:54 -0300

    erro 1

--- mvp_alzheimer_projeto/lib/MyHomePage.dart
+++ mvp_alzheimer_projeto/lib/MyHomePage.dart
@@ -6,6 +6,9 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:jiffy/jiffy.dart';
 import 'package:projeto_estudo/MyApp.dart';
+import 'package:projeto_estudo/src/components/CustomButton.dart';
+
+import 'src/controller/ProfileController.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -23,12 +26,15 @@ class _MyHomePageState extends State<MyHomePage> {
   final defaultPadding = 20.0;
   @override
   Widget build(BuildContext context) {
+     DateTime agora = DateTime.now();
+    print(
+        (agora.difference(ProfileController.instance.dataNasc).inDays / 365)
+            .toString()
+            .substring(0, 2));
     return Scaffold(
       // map dos botoes
-      backgroundColor: Colors.lightBlue,
-      appBar: AppBar(
-        title: Center(child: Text("Menu")),
-      ),
+      backgroundColor: Color.fromRGBO(121,188,218, 1),
+      appBar: CustomAppBar.instance.getNamedDefault(context, "",Center(child: Text('Menu'))),
       body: SizedBox(
         child: Column(
           children: [
@@ -50,37 +56,35 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
-            CircleAvatar(
+             CircleAvatar(
               backgroundColor: Colors.white,
-              radius: 60,
+              radius: 85,
               child: CircleAvatar(
-                radius: 80,
-                backgroundImage: AssetImage('assets/images/pelezin.jpg'),
-              ),
+            backgroundImage: ProfileController.instance.image,
+            radius: 100,
+          )
+          ),
+            Container(
+              height: 5,
             ),
+             Text(
+            "Nome: ${ProfileController.instance.nome}",
+            style: TextStyle(fontSize: 24,),
+          ),
             Container(
               height: 5,
             ),
             Text(
-              "Pele",
-              style: TextStyle(
-                fontSize: 24,
-              ),
-            ),
+            "Nascimento: ${Jiffy(ProfileController.instance.dataNasc).format("MMMM do yyyy")}",
+            style: TextStyle(fontSize: 24),
+          ),
             Container(
               height: 5,
             ),
             Text(
-              "Nascimento: 05/10/2001",
-              style: TextStyle(fontSize: 24),
-            ),
-            Container(
-              height: 5,
-            ),
-            Text(
-              "Idade: 35 anos",
-              style: TextStyle(fontSize: 24),
-            ),
+            "Idade: ${(agora.difference(ProfileController.instance.dataNasc).inDays / 365).toString().substring(0, 2)}",
+            style: TextStyle(fontSize: 20),
+          ),
             Container(
               height: 30,
             ),
@@ -89,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      primary: Colors.lightBlue, padding: EdgeInsets.all(15.0)),
+                      primary: Color.fromRGBO(121,188,218, 1), padding: EdgeInsets.all(15.0)),
                   onPressed: () {},
                   child: Ink.image(
                     image: AssetImage("assets/icons/Remedio.png"),
@@ -103,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
                     padding: EdgeInsets.all(15.0),
-                    primary: Colors.lightBlue,
+                    primary: Color.fromRGBO(121,188,218, 1),
                   ),
                   onPressed: () {
                     Navigator.of(context).pushNamed('/memories');
@@ -119,7 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.lightBlue,
+                    primary: Color.fromRGBO(121,188,218, 1),
                     padding: EdgeInsets.all(15.0),
                   ),
                   onPressed: () {
@@ -139,7 +143,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    primary: Colors.lightBlue,
+                    primary: Color.fromRGBO(121,188,218, 1),
                     padding: EdgeInsets.all(15.0),
                   ),
                   onPressed: () {
