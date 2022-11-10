b447e169cf10ef3a7d6d26d76a5ec5e542b09ba9
Henrique Gabriel <henrique.gabriel.de.oliveira43@gmail.com>
2022-11-10 15:23:03 -0300

    Remedio
    Acrecentado Data e Observação

--- mvp_alzheimer_projeto/lib/src/remedios/ModifyRemedio.dart
+++ mvp_alzheimer_projeto/lib/src/remedios/ModifyRemedio.dart
@@ -16,6 +16,8 @@ class _ModifyRemedio extends State<ModifyRemedio> {
   late TextEditingController controllerNome;
   late TextEditingController controllerDosagem;
   late TextEditingController controllerHora;
+  late TextEditingController controllerData;
+  late TextEditingController controllerObservacao;
 
   @override
   void initState() {
@@ -23,6 +25,8 @@ class _ModifyRemedio extends State<ModifyRemedio> {
     controllerNome = new TextEditingController(text: c.nome);
     controllerDosagem = new TextEditingController(text: c.dosagem);
     controllerHora = new TextEditingController(text: c.hora);
+    controllerData = new TextEditingController(text: c.data);
+    controllerObservacao = new TextEditingController(text: c.observacao);
     super.initState();
   }
 
@@ -30,25 +34,53 @@ class _ModifyRemedio extends State<ModifyRemedio> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppController.instance.mainColor,
-      appBar: CustomAppBar.instance.getNamedDefault(context,"/remedioPage",Text(" Remédio ",style: TextStyle(color: Colors.black),)),
+      appBar: CustomAppBar.instance.getNamedDefault(
+          context,
+          "/remedioPage",
+          Text(
+            " Remédio ",
+            style: TextStyle(color: Colors.black),
+          )),
       body: ListView(
         children: [
           TextBox(controllerNome, "Nome"),
           TextBox(controllerDosagem, "Dosagem"),
           TextBox(controllerHora, "Horario"),
+          TextBox(
+            controllerData,
+            "Quantidade de Dias",
+          ),
+          TextBox(
+            controllerObservacao,
+            "Observação",
+          ),
           ElevatedButton(
-              onPressed: () {
-                String nome = controllerNome.text;
-                String dosagem = controllerDosagem.text;
-                String hora = controllerHora.text;
+            onPressed: () {
+              String nome = controllerNome.text;
+              String dosagem = controllerDosagem.text;
+              String hora = controllerHora.text;
+              String data = controllerData.text;
+              String observacao = controllerObservacao.text;
 
-                if (nome.isNotEmpty && dosagem.isNotEmpty && hora.isNotEmpty) {
-                  Navigator.pop(context,
-                      new Remedio(nome: nome, dosagem: dosagem, hora: hora));
-                }
-              },
+              if (nome.isNotEmpty &&
+                  dosagem.isNotEmpty &&
+                  hora.isNotEmpty &&
+                  data.isNotEmpty &&
+                  observacao.isNotEmpty) {
+                Navigator.pop(
+                    context,
+                    new Remedio(
+                        nome: nome,
+                        dosagem: dosagem,
+                        hora: hora,
+                        data: data,
+                        observacao: observacao));
+              }
+            },
             style: ElevatedButton.styleFrom(primary: Colors.grey),
-            child: const Text("Salvar Remedio",style: TextStyle(color: Colors.black)),),
+            child: const Text("Salvar Remedio",
+                style: TextStyle(color: Colors.black)),
+          ),
         ],
       ),
     );
--- mvp_alzheimer_projeto/lib/src/remedios/RegisterRemedio.dart
+++ mvp_alzheimer_projeto/lib/src/remedios/RegisterRemedio.dart
@@ -14,41 +14,73 @@ class _RegisterRemedio extends State<RegisterRemedio> {
   late TextEditingController controllerNome;
   late TextEditingController controllerDosagem;
   late TextEditingController controllerHora;
+  late TextEditingController controllerData;
+  late TextEditingController controllerObservacao;
 
   @override
   void initState() {
     controllerNome = new TextEditingController();
     controllerDosagem = new TextEditingController();
     controllerHora = new TextEditingController();
+    controllerData = new TextEditingController();
+    controllerObservacao = new TextEditingController();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppController.instance.mainColor,
-        appBar: CustomAppBar.instance.getNamedDefault(context,"/remedioPage",Text(" Remédio ",style: TextStyle(color: Colors.black),)),
+        backgroundColor: AppController.instance.mainColor,
+        appBar: CustomAppBar.instance.getNamedDefault(
+            context,
+            "/remedioPage",
+            Text(
+              " Remédio ",
+              style: TextStyle(color: Colors.black),
+            )),
         body: ListView(
           children: [
             TextBox(controllerNome, "Nome"),
             TextBox(controllerDosagem, "Dosagem"),
-            TextBox(controllerHora, "Horario",),
-
+            TextBox(
+              controllerHora,
+              "Horario",
+            ),
+            TextBox(
+              controllerData,
+              "Quantidade de Dias",
+            ),
+            TextBox(
+              controllerObservacao,
+              "Observação",
+            ),
             ElevatedButton(
-                onPressed: () {
-                  String nome = controllerNome.text;
-                  String dosagem = controllerDosagem.text;
-                  String hora = controllerHora.text;
+              onPressed: () {
+                String nome = controllerNome.text;
+                String dosagem = controllerDosagem.text;
+                String hora = controllerHora.text;
+                String data = controllerData.text;
+                String observacao = controllerObservacao.text;
 
-                  if (nome.isNotEmpty &&
-                      dosagem.isNotEmpty &&
-                      hora.isNotEmpty) {
-                    Navigator.pop(context,
-                        new Remedio(nome: nome, dosagem: dosagem, hora: hora));
-                  }
-                },
+                if (nome.isNotEmpty &&
+                    dosagem.isNotEmpty &&
+                    hora.isNotEmpty &&
+                    data.isNotEmpty &&
+                    observacao.isNotEmpty) {
+                  Navigator.pop(
+                      context,
+                      new Remedio(
+                          nome: nome,
+                          dosagem: dosagem,
+                          hora: hora,
+                          data: data,
+                          observacao: observacao));
+                }
+              },
               style: ElevatedButton.styleFrom(primary: Colors.grey),
-                child: const Text("Salvar Remedio",style: TextStyle(color: Colors.black)),),
+              child: const Text("Salvar Remedio",
+                  style: TextStyle(color: Colors.black)),
+            ),
           ],
         ));
   }
