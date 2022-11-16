94329e0d7e17d0f0eb73a7085bc8954d7e6688a0
Henrique Gabriel <henrique.gabriel.de.oliveira43@gmail.com>
2022-11-16 13:12:06 -0300

    pequeno
    E isso

--- mvp_alzheimer_projeto/lib/src/remedios/remedioPaciente/RemedioPagePaciente.dart
+++ mvp_alzheimer_projeto/lib/src/remedios/remedioPaciente/RemedioPagePaciente.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 import 'package:projeto_estudo/src/remedios/RegisterRemedio.dart';
 import 'package:projeto_estudo/src/remedios/ViewRemedioPage.dart';
@@ -20,9 +21,18 @@ class _VisualizarRemedio extends State<VisualizarRemedio> {
   late TextEditingController controllerNome;
   late TextEditingController controllerDosagem;
   late TextEditingController controllerObservacao;
-  TimeOfDay controllerHora = TimeOfDay(hour: 0, minute: 00);
+  late TextEditingController controllerHorario;
+  dynamic teste = TimeOfDay.now();
+  String Hora = "";
+  dynamic horas;
+  dynamic teste2;
 
-  var index = 0;
+  String formatTimeOfDay(TimeOfDay tod) {
+    final now = new DateTime.now();
+    final dt = DateTime(now.year, now.month, now.day, tod.hour, tod.minute);
+    final format = DateFormat.jm(); //"6:00 AM"
+    return format.format(dt);
+  }
 
   @override
   void initState() {
@@ -30,6 +40,10 @@ class _VisualizarRemedio extends State<VisualizarRemedio> {
     controllerNome = new TextEditingController(text: c.nome);
     controllerDosagem = new TextEditingController(text: c.dosagem);
     controllerObservacao = new TextEditingController(text: c.observacao);
+    teste = c.hora;
+    horas = DateTime(teste.hour, teste.minute);
+    teste2 = DateFormat.jm();
+    Hora = teste2.format(horas).toString();
     super.initState();
   }
 
@@ -48,9 +62,13 @@ class _VisualizarRemedio extends State<VisualizarRemedio> {
         children: [
           TextBox(controllerNome, "Nome"),
           TextBox(controllerDosagem, "Dosagem"),
-          Text(AppController.instance.remedio[index].hora
-              .toString()
-              .substring(10, 15)),
+          Text(Hora,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 40,
+                wordSpacing: 20,
+                letterSpacing: 10,
+              )),
           TextBox(
             controllerObservacao,
             "Observação",
